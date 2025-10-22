d577be42dfbb9011c09fbafc19e94701ba2153d8
bbaskey2026 <bhimabaskey26@gmail.com>
2025-10-23 02:20:32 +0530

    Replace occurrence counting with median finding

--- prob5.cpp
+++ prob5.cpp
@@ -1,19 +1,42 @@
-#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
 
-int countOccurrences(string str, char ch) {
-    int count = 0;
-    for (char c : str) {
-        if (c == ch) {
-            count++;
-        }
+double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
+    if (nums1.size() > nums2.size()) return findMedianSortedArrays(nums2, nums1);
+
+    int m = nums1.size(), n = nums2.size();
+    int low = 0, high = m;
+
+    while (low <= high) {
+        int partitionX = (low + high) / 2;
+        int partitionY = (m + n + 1) / 2 - partitionX;
+
+        int maxX = (partitionX == 0) ? INT_MIN : nums1[partitionX - 1];
+        int minX = (partitionX == m) ? INT_MAX : nums1[partitionX];
+
+        int maxY = (partitionY == 0) ? INT_MIN : nums2[partitionY - 1];
+        int minY = (partitionY == n) ? INT_MAX : nums2[partitionY];
+
+        if (maxX <= minY && maxY <= minX) {
+            if ((m + n) % 2 == 0)
+                return (double)(max(maxX, maxY) + min(minX, minY)) / 2;
+            else
+                return (double)max(maxX, maxY);
+        } else if (maxX > minY)
+            high = partitionX - 1;
+        else
+            low = partitionX + 1;
     }
-    return count;
+
+    throw invalid_argument("Input arrays are not sorted");
 }
 
 int main() {
-    string str = "iloveprogramming";
-    char ch = 'g';
-    cout << "Occurrences of '" << ch << "': " << countOccurrences(str, ch) << endl;
-    return 0;
+    vector<int> nums1 = {1, 3};
+    vector<int> nums2 = {2};
+    cout << findMedianSortedArrays(nums1, nums2) << endl;
+
+    vector<int> nums3 = {1, 2};
+    vector<int> nums4 = {3, 4};
+    cout << findMedianSortedArrays(nums3, nums4) << endl;
 }
